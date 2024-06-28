c7dbb006d3463d27a79d8f82dbdc87ca8cf78443
Aditya <khuranaaditya067@gmail.com>
2024-06-28 15:46:24 +0530

    Your commit message

--- src/components/News.js
+++ src/components/News.js
@@ -2,61 +2,70 @@ import React, { Component } from 'react'
 import NewsItem from './NewsItem'
 
 export class News extends Component {
-    articles = [
-        {
-          "source": {
-            "id": null,
-            "name": "CBS Sports"
-          },
-          "author": "",
-          "title": "2024 NBA Mock Draft: Dalton Knecht surges up to No. 6, Bronny James goes to Lakers late in second round - CBS Sports",
-          "description": "Zaccharie Risacher edges Alex Carr for the top spot in Kyle Boone's two-round mock draft",
-          "url": "https://www.cbssports.com/nba/news/2024-nba-mock-draft-dalton-knecht-surges-up-to-no-6-bronny-james-goes-to-lakers-late-in-second-round/",
-          "urlToImage": "https://sportshub.cbsistatic.com/i/r/2024/06/25/8285b2ab-f385-47c7-a2d5-3d996e1b724e/thumbnail/1200x675/709dbe8138830227f08c94ede5601791/usatsi-22901377-1.jpg",
-          "publishedAt": "2024-06-26T06:18:00Z",
-          "content": "Months and months of speculation, analyzing (and overanalyzing) have all led to this. The 2024 NBA Draft will begin Wednesday night, where all the questions we have -- about who goes No. 1, about whe… [+814 chars]"
-        },
-        {
-          "source": {
-            "id": "fox-news",
-            "name": "Fox News"
-          },
-          "author": "Jamie Joseph",
-          "title": "John Curtis wins highly-contested primary Utah challenge to replace Sen. Mitt Romney - Fox News",
-          "description": "John Curtis emerged as the Republican champion to face off against Democratic nominee Caroline Gleich in Utah's open Senate race to replace Sen. Mitt Romney.",
-          "url": "https://www.foxnews.com/politics/john-curtis-wins-highly-contested-primary-utah-challenge-replace-sen-mitt-romney",
-          "urlToImage": "https://static.foxnews.com/foxnews.com/content/uploads/2024/06/John-Curtis-trent-staggs.jpg",
-          "publishedAt": "2024-06-26T02:30:00Z",
-          "content": "John Curtis won Tuesday night's primary challenge in Utah, taking one step closer to potentially replacing retiring moderate Republican Sen. Mitt Romney.\r\nCurtis was leading with 52% of the vote when… [+1906 chars]"
-        }
-      ]
+   
     constructor(){
         super();
         console.log("Hello i am i constructor from news component");
         this.state={
-            articles: this.articles,
-            loading: false
+            articles: [],
+            loading: false,
+            page:1
         }
     }
+
+    async componentDidMount(){
+        let url = " https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=9988d7d85825494a9ec530ff531009da&page=1pageSize=20"
+        let data = await fetch(url);
+        let parsedData = await data.json()
+        console.log(parsedData);
+        this.setState({articles: parsedData.articles, totalResults: parsedData.totalResults})
+    
+    }
+    handlePreviousClick = async ()=>{
+        let url = `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=9988d7d85825494a9ec530ff531009da&page=${this.state.page - 1}&pageSize=20`
+        let data = await fetch(url);
+        let parsedData = await data.json()
+        console.log(parsedData);
+        this.setState({articles: parsedData.articles})
+        this.setState({
+            page: this.state.page-1,
+            articles: parsedData.articles
+        })
+    }
+    handleNextClick = async()=>{
+        if(this.state.page+1 > Math.ceil(this.state.totalResults/20)){
+            
+        }
+        else{
+        let url = `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=9988d7d85825494a9ec530ff531009da&page=${this.state.page + 1}&pageSize=20`
+        let data = await fetch(url);
+        let parsedData = await data.json()
+        console.log(parsedData);
+        this.setState({articles: parsedData.articles})
+        this.setState({
+            page: this.state.page+1,
+            articles: parsedData.articles
+        })
+    } 
+}
 
   render() {
     return (
       <div className="container my-3">
-        <h2>newsApp top headlines</h2>
+        <h1>newsApp top headlines</h1>
         <div className= "row">
         {this.state.articles.map((element)=>{
           return <div className="col-md-4" key={element.url}>
-           <NewsItem  title={element.title.slice(0,45)} description={element.description.slice(0,100)} imageUrl={element.urlToImage} newsUrl={element.url}/>
-        </div>
-
-        })}
-            
-    
-        </div>
-          
-        
-        
-        
+           <NewsItem  title={element.title?element.title:""} description={element.description?element.description:""} imageUrl={element.urlToImage} newsUrl={element.url}/>
+         </div>
+         }
+         )
+        }
+        </div> 
+           <div className="container d-flex justify-content-between">
+           <button disabled={this.state.page<=1} type="button" className="btn btn-dark my-3" onClick={this.handlePreviousClick}> &larr; Previous</button>
+           <button type="button" className="btn btn-dark my-3"onClick={this.handleNextClick}>Next &rarr;</button>
+           </div>
       </div>
     )
   }
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -7,11 +7,11 @@ export class NewsItem extends Component {
     return (
       <div>
           <div className="card" style={{width: "18rem"}}>
-          <img src={imageUrl} className="card-img-top" alt="..."/>
+          <img src={!imageUrl?"https://sportshub.cbsistatic.com/i/r/2024/06/25/8285b2ab-f385-47c7-a2d5-3d996e1b724e/thumbnail/1200x675/709dbe8138830227f08c94ede5601791/usatsi-22901377-1.jpg":imageUrl} className="card-img-top" alt="..."/>
            <div className="card-body">
-               <h5 className="card-title">{title}...</h5>
-              <p className="card-text">{description}...</p>
-              <a href={newsUrl} target="_blank" className="btn btn-sm btn-primary">Read more</a>
+               <h5 className="card-title">{title}</h5>
+              <p className="card-text">{description}</p>
+              <a href={newsUrl} target="_blank" className="btn btn-sm btn-dark">Read more</a>
             </div>
         </div>
     </div>
